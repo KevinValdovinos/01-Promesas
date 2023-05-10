74d890665ec579dad096a55d1eb99928580ec214
Kevin Valdovinos <119825966+KevinValdovinos@users.noreply.github.com>
2023-05-09 20:52:09 -0600

    Add files via upload

---
+++ 02-promesas.js
@@ -0,0 +1,29 @@
+function sumarUnoPromesa(numero){
+    console.log(numero);
+    var promesa=new Promise(function(resolve,reject){
+        if(numero>=7){
+            reject("El numero es muy grande");
+            return;
+        }
+        setTimeout(function(){
+            resolve(numero+1) ;
+        },2000)
+    });
+
+    return promesa;
+}
+sumarUnoPromesa(5).then(sumarUnoPromesa)
+.then(sumarUnoPromesa)
+.then(sumarUnoPromesa)
+.catch((err)=>{
+    console.log(err);
+})
+// sumarUnoPromesa(5).then(function(nuevoValor){
+//     console.log(nuevoValor);
+//     sumarUnoPromesa(nuevoValor).then(function(nuevoValor){
+//         console.log(nuevoValor);
+//         sumarUnoPromesa(nuevoValor).then(function(nuevoValor){
+//             console.log(nuevoValor);
+//         })
+//     })
+// })
